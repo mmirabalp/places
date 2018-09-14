054bb6efbc0995b3c17e17ed1dcd5ce0ead641fe
Mawel Mirabal <mmirabalp@gmail.com>
2018-09-14 00:45:49 -0700

    Start

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  hello() {
+if(false){
+  return <p>:)</p>
+}else{
+  return <p>(:</p>
+}
+
+
+
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">
+            {this.hello()}
+          </h1>
+
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Places</h1>
+        <p>Find most places Easely</p>
+      </div>
+
+
+    );
+
+  };
+}
