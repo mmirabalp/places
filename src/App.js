272e802dc6edee08e88abc53ef85ce50c5cec934
Mawel Mirabal <mmirabalp@gmail.com>
2018-09-14 20:59:17 -0700

    Added some elements

--- src/App.js
+++ src/App.js
@@ -5,20 +5,50 @@ import Title from './components/Title';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0
+    };
+    this.updateNumber = this.updateNumber.bind(this);
+  }
+
+  updateNumber() {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Title></Title>
+      <section>
+        <div>
+          <div>
+            <Title></Title>
+            <h2>{this.state.number}</h2>
+            <button onClick={ this.updateNumber }>Free Registration</button>
+            <img src={process.env.PUBLIC_URL + '/images/Australia.jpg'} height="300px" />
+            <div>
+              <ul>
+                <li>
+                  <h3>Evaluate Places based in your Emotions</h3>
+                  <p>Evaluate your places with experiences, not with numbers</p>
+                </li>
+                <li>
+                  <h3>No internet, No Problem</h3>
+                  <p>Places work without internet or slow signal area</p>
+                </li>
+                <li>
+                  <h3>Your favorite Places</h3>
+                  <p>Define a list of your favorite places</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      </section>
+    )
   }
 }
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -9,5 +9,5 @@ export default class Title extends React.Component {
       </div>
 
     );
-  };
+  }
 }
