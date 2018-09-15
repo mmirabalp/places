bcf695e2dd834862f0d0a4477e2047902fbb9976
Mawel Mirabal <mmirabalp@gmail.com>
2018-09-14 23:39:28 -0700

    WIP_Added some styles

--- src/App.js
+++ src/App.js
@@ -1,53 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+// import logo from './logo.svg';
+import { indigo400 } from 'material-ui/styles/colors';
+
 import Title from './components/Title';
+import './App.css';
+
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0
-    };
-    this.updateNumber = this.updateNumber.bind(this);
-  }
 
-  updateNumber() {
-    this.setState({
-      number: this.state.number + 1
-    })
-  }
+
 
   render() {
     return (
-      <section>
-        <div>
-          <div>
-            <Title></Title>
-            <h2>{this.state.number}</h2>
-            <button onClick={ this.updateNumber }>Free Registration</button>
-            <img src={process.env.PUBLIC_URL + '/images/Australia.jpg'} height="300px" />
-            <div>
-              <ul>
-                <li>
-                  <h3>Evaluate Places based in your Emotions</h3>
-                  <p>Evaluate your places with experiences, not with numbers</p>
-                </li>
-                <li>
-                  <h3>No internet, No Problem</h3>
-                  <p>Places work without internet or slow signal area</p>
-                </li>
-                <li>
-                  <h3>Your favorite Places</h3>
-                  <p>Define a list of your favorite places</p>
-                </li>
-              </ul>
+      <MuiThemeProvider>
+        <div className="Header-background">
+          <div style={{ "width": "80%", "margin": "0 auto" }}>
+            <div className="Header-Main">
+              <Title></Title>
+
+              <RaisedButton label="Create free account" secondary={true} />
+              <img className="Header-ilustration" src={process.env.PUBLIC_URL + '/images/Australia.jpg'} height="300px" />
             </div>
           </div>
+          <div style={{ 'background-color': indigo400 }}>
+            <ul>
+              <li>
+                <h3>Evaluate Places based in your Emotions</h3>
+                <p>Evaluate your places with experiences, not with numbers</p>
+              </li>
+              <li>
+                <h3>No internet, No Problem</h3>
+                <p>Places work without internet or slow signal area</p>
+              </li>
+              <li>
+                <h3>Your favorite Places</h3>
+                <p>Define a list of your favorite places</p>
+              </li>
+            </ul>
+          </div>
         </div>
-
-      </section>
+      </MuiThemeProvider >
     )
   }
 }
