a6b5c0f60c231b8571f1752befc6409ff278394f
Mawel Mirabal <mmirabalp@gmail.com>
2018-09-18 23:05:36 -0700

    Added Some Components and Styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Card, CardText} from 'material-ui/Card'
-import {indigo400, redA400, lightBlueA400, amberA400} from 'material-ui/styles/colors';
+import {indigo400, redA400, lightBlueA400, amberA400, green500, greenA200, lime500} from 'material-ui/styles/colors';
 
 import Title from './components/Title';
 import './App.css';
@@ -20,7 +20,56 @@ class App extends Component {
             <Title></Title>
 
             <RaisedButton label="Create free account" secondary={true}/>
-          <img className="Header-ilustration" src={process.env.PUBLIC_URL + '/images/globe-map-icon.png'} height="300px"/>
+            <img className="Header-ilustration" src={process.env.PUBLIC_URL + '/images/globe-map-icon.png'} height="300px"/>
+          </div>
+          <div className="">
+            <ul>
+
+
+              <Card className="Header-Benefit">
+                <CardText>
+                  <div className="row">
+                    <div className="Header-Benefit-image" style={{
+                        'backgroundColor' : green500
+                      }}><img src={process.env.PUBLIC_URL + '/images/heart.png'}/></div>
+                    <div className="Header-Benefit-content">
+                      <h3>Places Emotional Evaluation.</h3>
+                      <p>Evaluate places base in your experience there</p>
+                    </div>
+                  </div>
+                </CardText>
+              </Card>
+
+              <Card className="Header-Benefit">
+                <CardText>
+                  <div className="row">
+                    <div className="Header-Benefit-image" style={{
+                        'backgroundColor' : greenA200
+                      }}><img src={process.env.PUBLIC_URL + '/images/no-Wifi.png'}/></div>
+                    <div className="Header-Benefit-content">
+                      <h3>No internet, No Problem</h3>
+                      <p>Places work without internet or slow signal area</p>
+                    </div>
+                  </div>
+                </CardText>
+              </Card>
+
+              <Card className="Header-Benefit">
+                <CardText>
+                  <div className="row">
+                    <div className="Header-Benefit-image" style={{
+                        'backgroundColor' : lime500
+                      }}><img src={process.env.PUBLIC_URL + '/images/star.png'}/></div>
+                    <div className="Header-Benefit-content">
+                      <h3>No internet, No Problem</h3>
+                      <p>Places work without internet or slow signal area</p>
+                    </div>
+                  </div>
+                </CardText>
+              </Card>
+
+
+            </ul>
           </div>
         </div>
 
@@ -29,40 +78,9 @@ class App extends Component {
           'background-color' : indigo400,
           'padding' : '50px'
         }}>
-        <ul>
-          <Card className="Header-Benefit">
-            <CardText style={{
-                'backgroundColor' : redA400
-              }}>
-              <img src={process.env.PUBLIC_URL + '/images/heart.png'}/>
-              <h3>Evaluate Places based in your Emotions</h3>
-              <p>Evaluate your places with experiences, not with numbers
-              </p>
-            </CardText>
-          </Card>
-          <Card className="Header-Benefit">
-            <CardText style={{
-                'backgroundColor' : redA400
-              }}>
-              <img src={process.env.PUBLIC_URL + '/images/no-Wifi.png'}/>
-              <h3>No internet, No Problem</h3>
-              <p>Places work without internet or slow signal area</p>
-            </CardText>
-          </Card>
-          <Card className="Header-Benefit">
-            <CardText style={{
-                'backgroundColor' : redA400
-              }}>
-              <img src={process.env.PUBLIC_URL + '/images/star.png'}/>
-              <h3>Your favorite Places</h3>
-              <p>Define a list of your favorite places</p>
-            </CardText>
-          </Card>
 
-        </ul>
       </div>
-    </MuiThemeProvider >)
+    </MuiThemeProvider>)
   }
 }
-
 export default App;
